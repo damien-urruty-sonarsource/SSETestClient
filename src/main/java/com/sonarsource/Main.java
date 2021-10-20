1d9f78b80c812ad81872bc1ac000c65621c6702f
Damien Urruty <damien.urruty@sonarsource.com>
2021-10-20 15:58:36 +0200

    Reconnect as soon as disconnected

--- src/main/java/com/sonarsource/Main.java
+++ src/main/java/com/sonarsource/Main.java
@@ -6,6 +6,14 @@ public class Main {
 
   public static void main(String[] args) throws Exception {
     ApacheHttpClient client = new ApacheHttpClient();
-    client.getStreamed(SERVER_API_URL, System.out::println);
+
+    do {
+      try {
+        client.getStreamed(SERVER_API_URL, System.out::println);
+      } catch(Exception e) {
+        // ignore and reconnect
+      }
+      System.out.println("reconnecting");
+    } while (true);
   }
 }
